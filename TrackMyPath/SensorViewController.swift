0772b392e45aea251c8be5fd28f365015d11d7db
Sridhar M <sridhar@imagin8ors.com>
2015-10-14 00:54:16 +0530

    with distance calculation

--- TrackMyPath/SensorViewController.swift
+++ TrackMyPath/SensorViewController.swift
@@ -12,6 +12,38 @@ import CoreMotion
 class SensorViewController: UIViewController {
     var manager :CMMotionManager?
     var queue :NSOperationQueue?
+    
+    var calibrationRequired:Bool = true
+    
+    //FIXME Move the points to a structure
+    var correctionX:Double = 0.0
+    var correctionY:Double = 0.0
+    var correctionZ:Double = 0.0
+    var correctionCount = 1024.0
+    var calibrationSeekBar = 0.0
+    
+    var maxX = 0.0
+    var maxY = 0.0
+    var maxZ = 0.0
+    var minX = -0.0
+    var minY = -0.0
+    var minZ = -0.0
+    
+    var currAX:Double = 0.0
+    var currAY:Double = 0.0
+    var prevAX:Double = 0.0
+    var prevAY:Double = 0.0
+    
+    var currVX:Double = 0.0
+    var currVY:Double = 0.0
+    var prevVX:Double = 0.0
+    var prevVY:Double = 0.0
+    
+    var currDX:Double = 0.0
+    var currDY:Double = 0.0
+    var prevDX:Double = 0.0
+    var prevDY:Double = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("checking the load function bound")
@@ -21,30 +53,68 @@ class SensorViewController: UIViewController {
         
         
         if self.manager!.deviceMotionAvailable {
-            let updateFrequency = 1.0 // sec
+            let updateFrequency = 0.0167// sec
             self.manager!.deviceMotionUpdateInterval = updateFrequency
-            self.manager!.startDeviceMotionUpdatesToQueue(queue!) {
+            self.manager?.startDeviceMotionUpdatesToQueue(  queue!)
+                {
                 (data:CMDeviceMotion?,error:NSError?) in
-                /*var xv0:Double = 0
-                var xv:Double = 0
-                var yv0:Double = 0
-                var yv:Double = 0
-                var x = 0.0
-                var y = 0.0*/
                 if let d = data{
-                    //print("Attitude : pitch :: \(self.degrees(d.attitude.pitch)) Roll :: \(self.degrees(d.attitude.roll)) Yaw :: \(self.degrees(d.attitude.yaw))")
-                   // print("Gyro : x :: \( (d.rotationRate.x)) y :: \( (d.rotationRate.y)) z :: \( (d.rotationRate.z))")
-                    print("GA : x :: \( Utils.rnd(d.gravity.x)) y :: \( Utils.rnd(d.gravity.y)) z :: \( Utils.rnd(d.gravity.z))")
-                    print("UA : x :: \( Utils.rnd(d.userAcceleration.x)) y :: \( Utils.rnd(d.userAcceleration.y)) z :: \( Utils.rnd(d.userAcceleration.z))")
-                   /* xv = MUtils.getVelocity(xv0,a:d.userAcceleration.x,t:updateFrequency)
-                    yv = MUtils.getVelocity(yv0,a:d.userAcceleration.y,t:updateFrequency)
-                    x += MUtils.getDisplacment(xv0,a:d.userAcceleration.x,t:updateFrequency)
-                    y += MUtils.getDisplacment(yv0,a:d.userAcceleration.y,t:updateFrequency)
-                    xv0 = xv
-                    yv0 = yv
-                    print("\(x),\(y)")*/
+                    //x,y,z = raw value
+                    let x = Utils.convertG2ms2(d.userAcceleration.x)
+                    let y = Utils.convertG2ms2(d.userAcceleration.y)
+                    let z = Utils.convertG2ms2(d.userAcceleration.z)
+                    
+                    if self.calibrationRequired {
+                        self.doCalibrate(x,y:y,z:y)
+                        return
+                    }
+                   // print("\( x + self.correctionX) ,\( y + self.correctionY),\( z + self.correctionZ) vs \( x - self.correctionX) ,\( y - self.correctionY),\( z - self.correctionZ)")
+                    //x1,y1,z1 - Normalized value
+                    let x1 = (x < self.maxX && x > self.minX) ? 0 : x
+                    let y1 = (y < self.maxY && y > self.minY) ? 0 : y
+                    if x1 != 0  || y1 != 0{
+                        self.currAX = x1
+                        self.currAY = y1
+                        //calculate velocity and distance
+                        self.currVX = self.prevVX + self.prevAX + ((self.currAX - self.prevAX)/2)
+                        self.currVY = self.prevVY + self.prevAY + ((self.currAY - self.prevAY)/2)
+                        
+                        self.currDX = self.prevDX + self.prevVX + ((self.currVX - self.prevVX)/2)
+                        self.currDY = self.prevDY + self.prevVY + ((self.currVY - self.prevVY)/2)
+                        
+                        self.prevAX = self.currAX
+                        self.prevAY = self.currAY
+                        
+                        self.prevVX = self.currVX
+                        self.prevVY = self.currVY
+                        
+                        self.prevDX = self.currDX
+                        self.prevDY = self.currDY
+                        
+                        print("\(Int(Utils.rnd(self.currDX,decimalPoint:0))) , \(Int(Utils.rnd(self.currDY,decimalPoint:0))) " )
+                    }
                 }
             }
+        }
+        
+    }
+    
+    func doCalibrate(x:Double,y:Double,z:Double){
+        self.correctionX += x
+        self.correctionY += y
+        self.correctionZ += z
+        if maxX < x { maxX = x }
+        if maxY < y { maxY = y }
+        if minX > x { minX = x }
+        if minY > y { minY = y }
+        self.calibrationSeekBar++
+        if self.calibrationSeekBar >= self.correctionCount{
+            self.calibrationRequired = false
+            self.correctionX /= Double(self.correctionCount)
+            self.correctionY /= Double(self.correctionCount)
+            self.correctionZ /= Double(self.correctionCount)
+            print("correction value \(self.correctionX),\(self.correctionY),\(self.correctionZ)")
+            
             
         }
         
@@ -54,6 +124,6 @@ class SensorViewController: UIViewController {
             mgr.stopDeviceMotionUpdates()
         }
     }
-    
+        
 }
 
--- TrackMyPath/Utils.swift
+++ TrackMyPath/Utils.swift
@@ -8,9 +8,13 @@
 
 import Foundation
 class Utils {
+    static let g2ms2 = 980.0
     static func rnd(val:Double, decimalPoint:Int = 2) -> Double{
         let mulFactor = pow(10.0,Double(decimalPoint))
         //print("\(mulFactor) \(val)")
         return round(val * mulFactor) / mulFactor
+    }
+    static func convertG2ms2(val:Double) -> Double{
+        return Utils.rnd(val * Utils.g2ms2)
     }
 }

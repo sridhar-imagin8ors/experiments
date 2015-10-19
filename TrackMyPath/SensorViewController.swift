3007e70d6c6162a506298260d5c2a3125fcf3090
Sridhar M <sridhar@imagin8ors.com>
2015-10-20 02:17:33 +0530

    1. yaw and y axis. 2. No z and x data. 3. Create straight lines using motion 4. ToDo : device position should be inline with the yaw to get the accurate direction. 5 - Currently code doesn't support angle and circular shapes.

--- TrackMyPath/SensorViewController.swift
+++ TrackMyPath/SensorViewController.swift
@@ -34,9 +34,10 @@ class SensorViewController: UIViewController , MFMailComposeViewControllerDelega
     var filename = "track_data.txt"
     var dumpdata:String = ""
     var yawDegree:Int = 0
+    let degreeBand:Int = 5
     
     
-    func doCalibrate(x:Double,y:Double,z:Double,yaw:int){
+    func doCalibrate(x:Double,y:Double,z:Double,yaw:Int){
         self.correction.x += x
         self.correction.y += y
         self.correction.z += z
@@ -54,7 +55,11 @@ class SensorViewController: UIViewController , MFMailComposeViewControllerDelega
             self.correction.x /= Double(self.correctionCount)
             self.correction.y /= Double(self.correctionCount)
             self.correction.z /= Double(self.correctionCount)
-            print("correction value \(self.correction.x),\(self.correction.y),\(self.correction.z)")
+            self.yawDegree /= Int(self.correctionCount)
+            //            if self.yawDegree < 0{
+            //                self.yawDegree + 360
+            //            }
+            print("correction value \(self.correction.x),\(self.correction.y),\(self.correction.z), y reference angle : \( self.yawDegree)")
             print("max value \(self.max.x),\(self.max.y),\(self.max.z)")
             print("min value \(self.min.x),\(self.min.y),\(self.min.z)")
             //mailTextLabel.text = " Calibrated. Lets go for a walk"
@@ -127,9 +132,11 @@ class SensorViewController: UIViewController , MFMailComposeViewControllerDelega
                     if let d = data{
                         //x,y,z = raw value
                         let a = Point(x: 0.0,y:Utils.convertG2ms2(d.userAcceleration.y), z:0.0)
+                        //print("\(Int(MUtils.radianToDegrees(d.attitude.yaw))) , \(d.attitude.yaw)")
                         //self.mailTextLabel.text = "Calibrating... Keep the device still"
                         if self.calibrationRequired {
-                            self.doCalibrate(a.x,y:a.y,z:a.z,Int(MUtils.radianToDegrees(d.attitude.yaw)))
+                            
+                            self.doCalibrate(a.x,y:a.y,z:a.z,yaw:Int(MUtils.radianToDegrees(d.attitude.yaw)))
                             return
                         }
                         //  self.mailTextLabel.text = "Calibration Completed.\n Now you move the device"
@@ -138,16 +145,54 @@ class SensorViewController: UIViewController , MFMailComposeViewControllerDelega
                         
                         //x1,y1,z1 - Normalized value
                         let x1 = (a.x + self.correction.x < self.max.x && a.x  + self.correction.x > self.min.x) ? 0 : a.x  + self.correction.x
-                        let y1 = (a.y + self.correction.y < self.max.y && a.y  + self.correction.y > self.min.y) ? 0 : a.y  + self.correction.y
+                        var y1 = (a.y + self.correction.y < self.max.y && a.y  + self.correction.y > self.min.y) ? 0 : a.y  + self.correction.y
                         let z1 = (a.z + self.correction.z < self.max.y && a.z  + self.correction.z > self.min.z) ? 0 : a.z  + self.correction.z
                         
                         if x1 != 0  || y1 != 0 || z1 != 0 {
-                            
-                            switch(MUtils.radianToDegrees(d.attitude.yaw)){
-                                case
+                            //                            let p = self.get45DegreeMinus(self.yawDegree)
+                            //                            let degreeNinty1:Int = self.get90Degree(p)[0]
+                            //                            let degreeNinty2:Int = self.get90Degree(p)[1]
+                            //                            let degreeOneEighty1:Int = self.get90Degree(degreeNinty1)[0]
+                            //                            let degreeOneEighty2:Int = self.get90Degree(degreeNinty1)[1]
+                            var yBy:Bool = true
+                            var isForward:Bool = true
+                            var currDegree = Int(MUtils.radianToDegrees(d.attitude.yaw)) + self.yawDegree
+                            currDegree = currDegree < 0 ? currDegree + 360 : currDegree
+                            if (45 > currDegree && currDegree >= 0 ) || (359 >= currDegree && currDegree >= 315){
+                                isForward = true
+                                yBy = true
+                                //print("y")
+                            } else if (135 <= currDegree && currDegree < 225){
+                                yBy = true
+                                isForward = false
+                                //print("-y")
+                            } else if (225 <= currDegree && currDegree < 315){
+                                yBy = false
+                                isForward = true
+                                //print("x")
+                            }else if (45 <= currDegree && currDegree < 135){
+                                isForward = false
+                                yBy = false
+                                print("-x")
+                            } else{
+                                print(" fixme - I am not suppose to be here  \(Int(MUtils.radianToDegrees(d.attitude.yaw)))" )
                             }
-                            self.acceleration[1].x = x1
-                            self.acceleration[1].y = y1
+                            
+                            if !isForward{
+                                y1 = abs(y1) * -1
+                            }else{
+                                y1 = abs(y1)
+                            }
+                            
+                            if yBy {
+                                self.acceleration[1].x = x1
+                                self.acceleration[1].y = y1
+                                
+                            } else {
+                                self.acceleration[1].x = y1
+                                self.acceleration[1].y = x1
+                                
+                            }
                             self.acceleration[1].z = z1
                             // print(" acceleration : \( a.dump() )  ")
                             //calculate velocity and distance
@@ -174,10 +219,10 @@ class SensorViewController: UIViewController , MFMailComposeViewControllerDelega
                                 self.velocity[0] = Point()
                                 self.acceleration[0] = Point()
                                 self.resetCount = 0
-                                print("reset")
+                                //print("reset")
                             }
                             if self.displacement[1].doesContainValue(){
-                                print("\(self.displacement[1].dump()),\(MUtils.radianToDegrees(d.attitude.yaw))")
+                                //print("\(self.displacement[1].dump()),\(MUtils.radianToDegrees(d.attitude.yaw))")
                                 self.dumpdata += self.displacement[1].dump() + "\n"
                             }
                             self.resetCount++
@@ -185,9 +230,10 @@ class SensorViewController: UIViewController , MFMailComposeViewControllerDelega
                             //no acceleration
                             self.resetCount++
                         }
-                     }
+                    }
             }
         }
     }
+    
 }
 
--- TrackMyPath/Utils.swift
+++ TrackMyPath/Utils.swift
@@ -17,4 +17,68 @@ class Utils {
     static func convertG2ms2(val:Double) -> Double{
         return Utils.rnd(val * Utils.g2ms2)
     }
+    
+    static func get90Degree(d:Int)->[Int]{
+        
+        var res:[Int] = [0,0]
+        if d < 0 {
+            if (d + 90 < 0){
+                res[0] = d + 90
+                res[1] = res[0] + 180
+            } else {
+                res[1] = d + 90
+                res[0] = res[1] + 180
+            }
+        } else {
+            if (d - 90 < 0){
+                res[0] = d - 90
+                res[1] = res[0] + 180
+            } else {
+                res[1] = d - 90
+                res[0] = res[1] - 180
+            }
+            
+        }
+        return res
+    }
+    static func get45DegreePlus(d:Int) -> Int{
+        let res = d + 45
+        return res > 180 ? (res - 360) : res
+    }
+    static func get45DegreeMinus(d:Int) -> Int{
+        let res = d - 44
+        return res < -180 ? (res + 360) : res
+    }
+    
+    static func isInside(low:Int, high:Int , deg: Int) -> Bool{
+        print(" \(low) \(high) \(deg)")
+        let deg = deg < 0 ? deg + 360 : deg
+        let l = low < 0 ? low + 360 : low
+        let h = high < 0 ? high + 360 : high
+        
+        if  l > h  { // high move to 0 degree and low is with in 360
+            if (deg >= 0 && deg <= h){
+                print("true")
+                return true
+            } else if (deg >= l && deg >= 359) {
+                print("true")
+                return true
+            } else if (deg >= h && deg <= l){
+                print("true")
+                return true
+                
+            } else {
+                print("false")
+                return false
+            }
+        } else{
+            if deg >= l && deg <= h {
+                print("true")
+                return true
+            }
+        }
+        print("false")
+        return false
+    }
+
 }
